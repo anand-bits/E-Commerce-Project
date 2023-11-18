92ab099776503d962b0ac616639f088987688bdd
anand-bits <h20220277@pilani.bits-pilani.ac.in>
2023-11-18 23:18:38 +0530

    register Kaam Kar rha

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,11 +1,12 @@
 import User from "../model/User.js";
 import AppError from "../utils/error.utils.js";
+import bcrypt from "bcrypt";
 
 // @desc  --- Register User
 // @route---- POST/api/v1/users/register
 //acess Private/admin
 
-
+// Registratoon Controller
 export const registerUserCtrl= async(req,res,next)=>
 {   
     const {fullname,email,password}=req.body;
@@ -17,9 +18,13 @@ export const registerUserCtrl= async(req,res,next)=>
         return next(new AppError("User exist with Email Id plese Login form that Id"),400);
 
     }
+    const salt=await bcrypt.genSalt(10);
+
+    const hashedPassword= await bcrypt.hash(password,salt)
 
     const user=await User.create({
-        fullname,email,password
+        fullname,email,
+        password:hashedPassword
     });
 
     res.status(200).json({
@@ -28,3 +33,23 @@ export const registerUserCtrl= async(req,res,next)=>
 
     })
 }
+
+// Login Controller
+
+export const loginUserCtrl= async(req,res,next)=>
+{
+    const {email,password}= req.body;
+
+    // First Verify the user exist or not---------
+
+    const userFound=await User.findOne({email});
+
+    if(!userFound)
+    {
+        return next(new AppError("User exist with Email Id plese Login form that Id"),400);
+
+    }
+
+}
+
+
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { registerUserCtrl } from "../controller/userCtrl.js";
+import { loginUserCtrl, registerUserCtrl } from "../controller/userCtrl.js";
 
 
 
 const userRoutes= express.Router();
 
 userRoutes.post('/register',registerUserCtrl)
+userRoutes.post('/login',loginUserCtrl);
+
 
 
 
