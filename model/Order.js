938c91892fddffa106e92f2e913b31cf4590ed86
anand-bits <h20220277@pilani.bits-pilani.ac.in>
2023-11-21 02:51:53 +0530

    Dummmt order basic is Created

--- model/Order.js
+++ model/Order.js
@@ -2,6 +2,8 @@
 import mongoose  from "mongoose";
 
 const Schema= mongoose.Schema;
+const randomTxt = Math.random().toString(36).substring(7).toLocaleUpperCase();
+const randomNumbers = Math.floor(1000 + Math.random() * 90000);
 
 const OrderSchema= new Schema({
     user:{
