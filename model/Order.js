58302b57ec2ad1b29176233b56ef374e5a3733da
anand-bits <h20220277@pilani.bits-pilani.ac.in>
2023-11-21 02:29:27 +0530

    Order Model completed

--- model/Order.js
+++ model/Order.js
@@ -29,6 +29,42 @@ const OrderSchema= new Schema({
     orderNumber:{
         type:String,
         required:true,
-        default:
-    }
-})
+        default:randomTxt+randomNumbers
+    },
+    paymentStatus:{
+        type:String,
+        required:true,
+        default:"Not paid"
+    },
+
+    paymentMethod:{
+        type:String,
+        default:"Not specified"
+    },
+    currency:{
+        type:String,
+        default:"Not specified",
+
+    },
+    //for admin
+    
+    status:{
+        type:String,
+        default:"Pending",
+        enum:["pendin","processing","shipped","deliverd"],
+
+    },
+    deliveredAt:{
+        type:Date
+    },
+},
+{
+    timestamps:true
+
+});
+
+
+// Compile To form the model
+
+const Order= mongoose.model("Order",OrderSchema)
+export default Order;
