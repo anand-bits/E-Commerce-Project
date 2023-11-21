180276683a3ed664aa77687c14aba3deb015288a
anand-bits <h20220277@pilani.bits-pilani.ac.in>
2023-11-21 14:18:59 +0530

    Order Creation Wokring fine till Here

--- controller/orderCtrl.js
+++ controller/orderCtrl.js
@@ -1,17 +1,58 @@
-import asyncHandler from "express-async-handler"
-import Order from "../model/Order.js"
+import asyncHandler from "express-async-handler";
+import Order from "../model/Order.js";
+import User from "../model/User.js";
+import Product from "../model/Product.js";
 
+// @description Create Order
+// @route POST /api/v1/orders
+// @access Private
+export const createOrderCtrl = asyncHandler(async (req, res) => {
+  try {
+    const { orderItems, shippingAddress, totalPrice } = req.body;
+    console.log(req.body);
+    //Find the user
+    const user = await User.findById(req.userAuthId);
+    //Check if user has shipping address
+   
+    //Check if order is not empty
+    if (orderItems?.length <= 0) {
+      throw new Error("No Order Items");
+    }
+    //Place/create order - save into DB
+    const order = await Order.create({
+      user: user?._id,
+      orderItems,
+      shippingAddress,
+      // totalPrice: couponFound ? totalPrice - totalPrice * discount : totalPrice,
+      totalPrice,
+    });
+  
+    const products = await Product.find({ _id: { $in: orderItems } });
 
-//@description Create Order
-// @Route Post/ api/v1/orders
-//access Private
-
-export const createOrderCtrl= asyncHandler(async(req,res)=>{
-
+  orderItems?.map(async (order) => {
+    const product = products?.find((product) => {
+      return product?._id?.toString() === order?._id?.toString();
+    });
+    if (product) {
+      product.totalSold += order.qty;
+    }
+    await product.save();
+  });
+  //push order into user
+  user.orders.push(order?._id);
+  await user.save();
+    // Additional steps for payment, webhook, etc.
+    // ...
 
     res.status(200).json({
-        success:true,
-        message:"Order Created "
-    })
-
-})
+      success: true,
+      message: "Order created successfully",
+      order,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
--- model/Order.js
+++ model/Order.js
@@ -1,72 +1,83 @@
+import mongoose from 'mongoose';
 
-import mongoose  from "mongoose";
+const Schema = mongoose.Schema;
 
-const Schema= mongoose.Schema;
-const randomTxt = Math.random().toString(36).substring(7).toLocaleUpperCase();
-const randomNumbers = Math.floor(1000 + Math.random() * 90000);
-
-const OrderSchema= new Schema({
-    user:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        required:true,
-
-    },
-
-    orderItems:[
-        {
-        type:Object,
-        required:true,
-        }
-
-    ],
-
-    shippingAddress:{
-        type:Object,
-        required:true,
-
-
-    },
-
-    orderNumber:{
-        type:String,
-        required:true,
-        default:randomTxt+randomNumbers
-    },
-    paymentStatus:{
-        type:String,
-        required:true,
-        default:"Not paid"
-    },
-
-    paymentMethod:{
-        type:String,
-        default:"Not specified"
-    },
-    currency:{
-        type:String,
-        default:"Not specified",
-
-    },
-    //for admin
-    
-    status:{
-        type:String,
-        default:"Pending",
-        enum:["pendin","processing","shipped","deliverd"],
-
-    },
-    deliveredAt:{
-        type:Date
-    },
-},
-{
-    timestamps:true
-
+// Order Item Schema
+const OrderItemSchema = new Schema({
+  _id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product', // Assuming your product model is named 'Product'
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  qty: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
 });
 
+// Order Schema
+const OrderSchema = new Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    orderItems: {
+      type: [OrderItemSchema],
+      required: true,
+    },
+    shippingAddress: {
+      type: Object,
+      required: true,
+    },
+    orderNumber: {
+      type: String,
+      default: () => {
+        const randomTxt = Math.random().toString(36).substring(7).toLocaleUpperCase();
+        const randomNumbers = Math.floor(1000 + Math.random() * 90000);
+        return randomTxt + randomNumbers;
+      },
+    },
+    paymentStatus: {
+      type: String,
+      default: 'Not paid',
+    },
+    paymentMethod: {
+      type: String,
+      default: 'Not specified',
+    },
+    totalPrice: {
+      type: Number,
+      default: 0.0,
+    },
+    currency: {
+      type: String,
+      default: 'Not specified',
+    },
+    status: {
+      type: String,
+      default: 'pending',
+      enum: ['pending', 'processing', 'shipped', 'delivered'],
+    },
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-// Compile To form the model
+// Compile to form model
+const Order = mongoose.model('Order', OrderSchema);
 
-const Order= mongoose.model("Order",OrderSchema)
 export default Order;
