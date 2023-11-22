3e1e67ae7902c9acc20d451700b7d8f2c10b5878
anand-bits <h20220277@pilani.bits-pilani.ac.in>
2023-11-22 22:04:50 +0530

    Avg and Today Admins sale are shown there

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,6 +1,6 @@
 import Router from "express";
 import { isLoggedIn } from "../middleware/isLoggedIn.js";
-import { createOrderCtrl, getAllordersCtrl, getSingleOrderCtrl, updateOrderCtrl } from "../controller/orderCtrl.js";
+import { createOrderCtrl, getAllordersCtrl, getOrderStatsCtrl, getSingleOrderCtrl, updateOrderCtrl } from "../controller/orderCtrl.js";
 
 
 const orderRouter= Router();
@@ -9,6 +9,7 @@ orderRouter.post("/",isLoggedIn,createOrderCtrl)
 orderRouter.get("/",isLoggedIn,getAllordersCtrl);
 orderRouter.get("/:id",isLoggedIn,getSingleOrderCtrl);
 orderRouter.put("/update/:id",isLoggedIn,updateOrderCtrl)
+orderRouter.get("/sales/sum",isLoggedIn,getOrderStatsCtrl)
 
 
 export default orderRouter;
